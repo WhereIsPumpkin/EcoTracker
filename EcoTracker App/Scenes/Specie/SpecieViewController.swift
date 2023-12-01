884bfdd598a66b14da6967b8b5d1fe54d6466f26
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2023-12-01 17:40:50 +0400

    fix: add final to tableview cell + improve code readability

--- EcoTracker App/Scenes/Specie/SpecieViewController.swift
+++ EcoTracker App/Scenes/Specie/SpecieViewController.swift
@@ -17,12 +17,10 @@ final class SpecieViewController: UIViewController {
     private lazy var tableView: UITableView = {
         let table = UITableView(frame: view.bounds, style: .grouped)
         table.register(SpecieTableViewCell.self, forCellReuseIdentifier: "cell")
-        table.delegate = self
-        table.dataSource = self
         return table
     }()
     
-    private var searchController: UISearchController! = {
+    private let searchController: UISearchController! = {
         let controller = UISearchController(searchResultsController: nil)
         controller.searchBar.placeholder = "Enter city (e.g. Tbilisi)"
         controller.hidesNavigationBarDuringPresentation = false
@@ -36,7 +34,7 @@ final class SpecieViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Specie"
-        specieViewModel.delegate = self
+        setDelegates()
         setupUI()
     }
     
@@ -49,6 +47,8 @@ final class SpecieViewController: UIViewController {
     
     private func setupTableView() {
         view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     private func setupSearchController() {
@@ -63,6 +63,10 @@ final class SpecieViewController: UIViewController {
             action: #selector(searchButtonTapped)
         )
         navigationItem.titleView = searchController.searchBar
+    }
+    
+    private func setDelegates() {
+        specieViewModel.delegate = self
     }
     
     // MARK: - Methods
@@ -80,16 +84,12 @@ final class SpecieViewController: UIViewController {
     }
 }
 
-// MARK: - UITableViewDataSource, UITableViewDelegate
-extension SpecieViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        // Adjust this value based on your desired cell height
-        return 100.0
-    }
+// MARK: - Extensions
+
+extension SpecieViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        species.count
+        return species.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -101,6 +101,13 @@ extension SpecieViewController: UITableViewDataSource, UITableViewDelegate {
         cell.configure(with: specie)
         
         return cell
+    }
+}
+
+extension SpecieViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100.0
     }
 }
 
--- EcoTracker App/Scenes/Specie/TableViewCell/SpecieTableViewCell.swift
+++ EcoTracker App/Scenes/Specie/TableViewCell/SpecieTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SDWebImage
 
-class SpecieTableViewCell: UITableViewCell {
+final class SpecieTableViewCell: UITableViewCell {
     
     // MARK: - UI Elements
     private let speciesImageView: UIImageView = {
